aa558a8e9336abc68cbff69bf20e5c1cd0aa90d8
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-05 14:48:53 -0500

    Clean up deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -7,28 +7,32 @@ module.exports = function(deployTarget) {
     },
     pipeline: {
       // This setting runs the ember-cli-deploy activation hooks on every deploy
-      // which is necessary in order to run ember-cli-deploy-cloudfront.
-      // To disable CloudFront invalidation, remove this setting or change it to `false`.
-      // To disable ember-cli-deploy-cloudfront for only a particular environment, add
-      // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
+      // which is necessary for ember-cli-deploy-s3-index to activate the index.html.
       activateOnDeploy: true
     },
     "revision-data": {
       "type": "version-commit"
     },
-    s3: {},
-    "s3-index": {}
+    s3: {
+      accessKeyId: process.env.ASSETS_PRODUCTION_AWS_KEY,
+      secretAccessKey: process.env.ASSETS_PRODUCTION_AWS_SECRET,
+      bucket: process.env.ASSETS_PRODUCTION_BUCKET,
+      region: process.env.ASSETS_PRODUCTION_REGION
+    },
+    "s3-index": {
+      accessKeyId: process.env.INDEX_PRODUCTION_AWS_KEY,
+      secretAccessKey: process.env.INDEX_PRODUCTION_AWS_SECRET,
+      bucket: process.env.INDEX_PRODUCTION_BUCKET,
+      region: process.env.INDEX_PRODUCTION_REGION
+    }
   };
 
+  if (deployTarget === 'staging') {
+    // Add staging-specific deploy configuration
+  }
+
   if (deployTarget === 'production') {
-    ENV.s3.accessKeyId = process.env.ASSETS_PRODUCTION_AWS_KEY;
-    ENV.s3.secretAccessKey = process.env.ASSETS_PRODUCTION_AWS_SECRET;
-    ENV.s3.bucket = process.env.ASSETS_PRODUCTION_BUCKET;
-    ENV.s3.region = process.env.ASSETS_PRODUCTION_REGION;
-    ENV["s3-index"].accessKeyId = process.env.INDEX_PRODUCTION_AWS_KEY;
-    ENV["s3-index"].secretAccessKey = process.env.INDEX_PRODUCTION_AWS_SECRET;
-    ENV["s3-index"].bucket = process.env.INDEX_PRODUCTION_BUCKET;
-    ENV["s3-index"].region = process.env.INDEX_PRODUCTION_REGION;
+    // Add production-specific deploy configuration
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
