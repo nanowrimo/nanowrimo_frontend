f794c014b6cc5c4f60ba7e695a3d50dfafc42f31
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-06 14:32:37 -0500

    Use router service to transition after sign up

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,13 +1,20 @@
 import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  router: service(),
+
   user: alias('model'),
 
   actions: {
     createUser() {
       let user = this.get('user');
-      user.save();
+      user.save().then(() => {
+        this.get('router').transitionTo('index');
+      }).catch((error) => {
+        alert(error);
+      });
     }
   }
 });
