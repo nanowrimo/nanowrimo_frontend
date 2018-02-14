bcbf3f401d4fd28f92937dce64e10d0e022c2a8b
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-14 16:13:25 -0500

    Disable jQuery

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function(defaults) {
       bootstrapVersion: 4,
       importBootstrapFont: false,
       importBootstrapCSS: false
+    },
+    vendorFiles: {
+      'jquery.js': null
     }
   });
 
