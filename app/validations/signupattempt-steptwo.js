f8e6c2c9a5408480a1858c2b48b3cd3f14216216
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-21 14:46:00 -0500

    Validate time zone is valid option

--- app/validations/signupattempt-steptwo.js
+++ app/validations/signupattempt-steptwo.js
@@ -1,12 +1,14 @@
 import {
+  validateInclusion,
   validatePresence
 } from 'ember-changeset-validations/validators';
+import TimeZones from 'nanowrimo/lib/time-zones';
 
 export default {
   username: [
     validatePresence(true),
   ],
   timeZone: [
-    validatePresence(true),
+    validateInclusion({ list: TimeZones.map(z => z.value) })
   ]
 };
