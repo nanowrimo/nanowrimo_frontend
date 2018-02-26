3b8a5403bb4056277c7d012d09a4ab6503a450a3
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-26 13:54:14 -0500

    Save uniqueness timeouts on a per-key basis

--- app/validators/uniqueness.js
+++ app/validators/uniqueness.js
@@ -4,13 +4,13 @@ import fetch from 'fetch';
 import ENV from 'nanowrimo/config/environment';
 
 const DEBOUNCE_MS = ENV.APP.DEBOUNCE_MS;
-let db_timeout;
+let db_timeout = {}; // Store timeouts on per-key basis so different keys don't cancel each other
 
 export default function validateUniqueness(remoteKey) {
   return (key, newValue, oldValue, changes, content) => { // eslint-disable-line no-unused-vars
     return new Promise((resolve) => {
-      clearTimeout(db_timeout);
-      db_timeout = setTimeout(() => {
+      clearTimeout(db_timeout[remoteKey]);
+      db_timeout[remoteKey] = setTimeout(() => {
         let url = `${ENV.APP.API_HOST}/uniqueness?${remoteKey}=${newValue}`;
 
         return fetch(url).then((response) => {
