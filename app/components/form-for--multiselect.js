9a2c8c780aa0f9db3c34bf539ddaa999836a16d1
Kevin Pfefferle <kevin@pfefferle.co>
2018-03-07 13:07:01 -0500

    Make multiselect more reusable

--- app/components/form-for--multiselect.js
+++ app/components/form-for--multiselect.js
@@ -1,42 +1,47 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { isNone } from '@ember/utils';
+import { isNone, isPresent } from '@ember/utils';
 
 export default Component.extend({
   store: service(),
 
-  errorMessage: '',
+  createOptionModel: null,
   hasAttemptedSubmit: false,
+  label: '',
   model: null,
+  options: null,
   property: '',
+  searchField: '',
 
-  genres: computed(function() {
-    return this.get('store').findAll('genre');
-  }),
-
-  selectedGenres: computed('model', 'property', function() {
+  _selectedOptions: computed('model', 'property', function() {
     return this.get(`model.${this.get('property')}`);
   }),
 
-  unselectedGenres: computed('genres.[]', 'selectedGenres.[]', function() {
-    let selectedGenres = this.get('selectedGenres');
-    return this.get('genres').reject((genre) => {
-      return selectedGenres.indexOf(genre) !== -1;
+  unselectedOptions: computed('options.[]', '_selectedOptions.[]', function() {
+    let selectedOptions = this.get('_selectedOptions');
+    return this.get('options').reject((option) => {
+      return selectedOptions.indexOf(option) !== -1;
     });
   }),
 
   actions: {
-    createGenre(value) {
-      let genre = this.get('genres').findBy('name', value);
-      if (isNone(genre)) {
-        genre = this.get('store').createRecord('genre', { name: value });
+    createOption(value) {
+      let createOptionModel = this.get('createOptionModel');
+      if (isPresent(createOptionModel)) {
+        let searchField = this.get('searchField');
+        let option = this.get('options').findBy(searchField, value);
+        if (isNone(option)) {
+          let props = {};
+          props[searchField] = value;
+          option = this.get('store').createRecord(createOptionModel, props);
+        }
+        this.get(`model.${this.get('property')}`).pushObject(option);
       }
-      this.get(`model.${this.get('property')}`).pushObject(genre);
     },
 
     showCreateWhen(value) {
-      return isNone(this.get('genres').findBy('name', value));
+      return isNone(this.get('options').findBy(this.get('searchField'), value));
     }
   }
 });
--- app/controllers/authenticated/projects/new.js
+++ app/controllers/authenticated/projects/new.js
@@ -1,11 +1,17 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   router: service(),
+  store: service(),
 
   project: alias('model'),
+
+  genreOptions: computed(function() {
+    return this.get('store').findAll('genre');
+  }),
 
   actions: {
     afterSubmit() {
