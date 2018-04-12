ca6d365d74c32ce28cea7e3028f0d2d150bceee2
jezra <jezra@jezra.net>
2018-04-12 15:25:46 -0700

    Refactored show_navigation property correctly imported {computed}

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,15 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   session: service(),
-  show_navigation: Ember.computed('routeName', function() {
+  show_navigation: computed('routeName', function() {
+    let no_nav_routes = ["sign-in","sign-up"];
     let rn = this.get('routeName');
-    return (rn!="sign-in" && rn!="sign-up");
+    return !no_nav_routes.includes(rn);
   }),
-  routeName: Ember.computed('currentPath', function() {
+  routeName: computed('currentPath', function() {
     return this.get('currentPath');
   }),
   actions: {
