18f33cf17262fd58ca192a640a65ae3e80057ea3
nanodave <dave_beck@nanowrimo.org>
2018-04-12 12:21:58 -0700

    Adding nav back in

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   session: service(),
-
+  showNavigation: true,
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -2,10 +2,6 @@ import Route from '@ember/routing/route';
 import UnauthenticatedRouteMixin from 'nanowrimo/mixins/unauthenticated-route';
 
 export default Route.extend(UnauthenticatedRouteMixin, {
-  setupController() {
-    this.controllerFor('application').set('show-navigation', false);
-  },
-  
   model() {
     return this.store.createRecord('sign-in-attempt');
   }
