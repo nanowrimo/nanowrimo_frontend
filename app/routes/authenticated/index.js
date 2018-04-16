4baaa0e89d0e5b845f4813b2db9ea3f3e7a0476e
nanodave <dave_beck@nanowrimo.org>
2018-04-16 09:41:01 -0700

    Partial styling on dashboard

--- app/routes/authenticated/index.js
+++ app/routes/authenticated/index.js
@@ -6,22 +6,15 @@ export default Route.extend({
       panels: [
         {
           cards: [
-            { title: "Card 1" },
-            { title: "Card 2" },
-            { title: "Card 3" }
-          ]
-        },
-        {
-          cards: [
-            { title: "Card 4" },
-            { title: "Card 5" }
+            { title: "My Badges", class: "g3-col1 g3-row1 g3-hspan2 g3-vspan1 g2-col1 g2-row1 g2-hspan2 g2-vspan1", templateName: "authenticated/projects" },
+            { title: "My Buddies", class: "g3-col3 g3-row1 g3-hspan1 g3-vspan2 g2-col2 g2-row3 g2-hspan1 g2-vspan2", templateName: "authenticated/projects" },
+            { title: "Regions", class: "g3-col1 g3-row2 g3-hspan2 g3-vspan1 g2-col1 g2-row2 g2-hspan2 g2-vspan1", templateName: "authenticated/projects" },
+            { title: "Writer's Resources", class: "g3-col1 g3-row3 g3-hspan1 g3-vspan1 g2-col1 g2-row3 g2-hspan1 g2-vspan1", templateName: "authenticated/projects" },
+            { title: "NaNo Store", class: "g3-col2 g3-row3 g3-hspan1 g3-vspan1 g2-col1 g2-row4 g2-hspan1 g2-vspan1", templateName: "authenticated/projects" },
+            { title: "My Offers", class: "g3-col3 g3-row3 g3-hspan1 g3-vspan1 g2-col1 g2-row5 g2-hspan1 g2-vspan1", templateName: "authenticated/projects" }
           ]
         }
       ]
     };
-  },
-  afterModel() {
-    // Hide the navigation when on the sign-in page
-    this.controllerFor('application').set('show_navigation', true);
   }
 });
