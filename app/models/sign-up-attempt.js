5cd8f73c4a76f421b6cb338e312dc9d1236192e6
jezra <jezra@jezra.net>
2018-04-17 10:25:33 -0700

    Broken checkbox stuff

--- app/models/sign-up-attempt.js
+++ app/models/sign-up-attempt.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   email: DS.attr('string', { defaultValue: '' }),
   password: DS.attr('string', { defaultValue: '' }),
   username: DS.attr('string', { defaultValue: '' }),
+  thirteen: DS.attr('boolean',{ defaultValue: false } ),
   timeZone: DS.attr('string', {
     defaultValue() { return moment.tz.guess(); }
   })
