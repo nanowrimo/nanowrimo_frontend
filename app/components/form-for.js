e4e82744f5d4a2475cf1bd8e6342ef522d0b90c1
jezra <jezra@jezra.net>
2018-04-17 10:50:49 -0700

    bj

--- app/components/form-for.js
+++ app/components/form-for.js
@@ -15,6 +15,7 @@ export default Component.extend({
   _currentStepIsValid: computed('changeset.error', 'currentStepIndex', 'steps.[]', function() {
     let propertiesForStep = this.get('steps').objectAt(this.get('currentStepIndex'));
     if (propertiesForStep) {
+      console.log(propertiesForStep);
       return propertiesForStep.every((property) => {
         return this.get(`changeset.error.${property}`) === undefined;
       });
