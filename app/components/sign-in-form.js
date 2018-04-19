1ca3834aafb89368a49075584c15431e66dc9009
jezra <jezra@jezra.net>
2018-04-19 11:51:18 -0700

    Removed unused "alias" in sign-in-form fixed sign-up-test to reference button

--- app/components/sign-in-form.js
+++ app/components/sign-in-form.js
@@ -1,9 +1,7 @@
 import Component from '@ember/component';
-import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  router: service(),
   session: service(),
   store: service(),
   error: null,
