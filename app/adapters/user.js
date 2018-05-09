84858e63ec4cab865cdd0b0f8f8884e1399a7aca
jezra <jezra@jezra.net>
2018-05-09 12:50:34 -0700

    Execute _super prior to fetching data
    fixes bug where fetching /user/current is not added authorization header

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -3,10 +3,9 @@ import fetch from 'fetch';
 
 export default ApplicationAdapter.extend({
   queryRecord(store, type, query) {
+    this._super(...arguments);
     if (query.current) {
       return fetch(`${this.urlPrefix()}/users/current`).then(response =>  response.json());
     }
-
-    return this._super(...arguments);
   }
 });
