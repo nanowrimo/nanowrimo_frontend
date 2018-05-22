35447adb8e84163122be45d6605fda5194c0cff2
Kevin Pfefferle <kevin@pfefferle.co>
2018-05-22 14:48:36 -0400

    Edit User stats

--- app/components/user-edit-profile-modal.js
+++ app/components/user-edit-profile-modal.js
@@ -1,10 +1,26 @@
 import Component from '@ember/component';
+import { computed }  from '@ember/object';
 
 export default Component.extend({
   tagName: '',
 
+  tab: null,
   open: false,
   user: null,
+
+  activeTab: computed('tab', {
+    get() {
+      return this.get('tab') || 'overview';
+    },
+    set(key, value) {
+      if (value === 'overview') {
+        this.set('tab', null);
+      } else {
+        this.set('tab', value);
+      }
+      return value;
+    }
+  }),
 
   actions: {
     onHidden() {
--- app/controllers/authenticated/users/show.js
+++ app/controllers/authenticated/users/show.js
@@ -7,9 +7,10 @@ export default Controller.extend({
   currentUser: service(),
   router: service(),
 
-  queryParams: ['edit'],
+  queryParams: ['edit', 'editTab'],
 
   edit: null,
+  editTab: null,
   userParam: null,
 
   user: alias('model'),
@@ -25,11 +26,21 @@ export default Controller.extend({
   actions: {
     afterModalClose() {
       this.set('edit', null);
+      this.set('editTab', null);
+
       if (this._needsURLUpdate()) {
         let userSlug = this.get('user.slug');
         let newURL = this.get('router.currentURL').replace(this.get('userParam'), userSlug);
         this.set('userParam', userSlug);
         this.get('router').replaceWith(newURL);
+      }
+    },
+
+    openEditModal(tab) {
+      if (this.get('canEditUser')) {
+        this.set('edit', true);
+
+        if (tab) { this.set('editTab', tab); }
       }
     }
   }
