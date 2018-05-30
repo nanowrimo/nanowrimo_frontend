b29c704a55725f564f109b5d583610f1b5d93db3
jezra <jezra@jezra.net>
2018-05-30 11:35:23 -0700

    Added missing route for project listing

---
+++ app/routes/authenticated/users/show/projects/index.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    let user = this.modelFor('authenticated.users.show');
+    return this.get('store').query('project', {
+      filter: { user_id: user.id},
+      include: 'genres,challenges'}
+    ).then(function(projects){
+      return projects;
+    });
+  }
+});
