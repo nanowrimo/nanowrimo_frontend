f3fe716cb8de796ecdfc01677abb997a0e4e1363
nanodave <dave_beck@nanowrimo.org>
2018-06-04 12:18:02 -0700

    Fixed build errors

--- app/components/dashboard-fundometer.js
+++ app/components/dashboard-fundometer.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { get,computed } from '@ember/object';
-import { reads }  from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
