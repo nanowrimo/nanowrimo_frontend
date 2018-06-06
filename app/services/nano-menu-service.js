ad9d8a2347632da2dd5cfdcf52fafc7bbe182bb0
nanodave <dave_beck@nanowrimo.org>
2018-06-06 11:14:40 -0700

    Fixed nav links; cleaned up images directory

--- app/services/nano-menu-service.js
+++ app/services/nano-menu-service.js
@@ -15,27 +15,27 @@ export default Service.extend({
       {
         toggleLabel: "My NaNoWriMo",
         submenuItems: [
-          {label: "Profile", url: "authenticated.users.show.index", segment: get(this,"currentUserName"), teaser: "Tell other Wrimos about you", src: "/nav/id_card.svg"},
-          {label: "Stats", url: "components.stats", segment: null, teaser: "Track your writing progress", src: "/nav/bar_chart.svg"},
-          {label: "Projects", url: "authenticated.users.show.projects", segment: get(this,"currentUserName"), teaser: "Organize all your projects", src: "/nav/open_book.svg"},
-          {label: "Buddies", url: "authenticated.users.show.buddies", segment: get(this,"currentUserName"), teaser: "Support and be supported", src: "/nav/clapping_hands.svg"}
+          {label: "Profile", url: "authenticated.users.show.index", segment: get(this,"currentUserName"), teaser: "Tell other Wrimos about you", src: "/images/nav/id_card.svg"},
+          {label: "Stats", url: "components.stats", segment: null, teaser: "Track your writing progress", src: "/images/nav/bar_chart.svg"},
+          {label: "Projects", url: "authenticated.users.show.projects", segment: get(this,"currentUserName"), teaser: "Organize all your projects", src: "/images/nav/open_book.svg"},
+          {label: "Buddies", url: "authenticated.users.show.buddies", segment: get(this,"currentUserName"), teaser: "Support and be supported", src: "/images/nav/clapping_hands.svg"}
         ]
       },
       {
         toggleLabel: "Community",
         submenuItems: [
-          {label: "Forums", url: "index", segment: null, teaser: "Our lively discussion space", src: "/nav/smiley_paper.svg"},
-          {label: "New York City", url: "index", segment: null, teaser: "Talk to WriMos near you", src: "/nav/map_pin.svg"},
-          {label: "Find a Region", url: "index", segment: null, teaser: "Join a region for more support", src: "/nav/earth.svg"},
-          {label: "Word Sprints", url: "index", segment: null, teaser: "Ready... set... write", src: "/nav/pencil_flag.svg"}
+          {label: "Forums", url: "index", segment: null, teaser: "Our lively discussion space", src: "/images/nav/smiley_paper.svg"},
+          {label: "New York City", url: "index", segment: null, teaser: "Talk to WriMos near you", src: "/images/nav/map_pin.svg"},
+          {label: "Find a Region", url: "index", segment: null, teaser: "Join a region for more support", src: "/images/nav/earth.svg"},
+          {label: "Word Sprints", url: "index", segment: null, teaser: "Ready... set... write", src: "/images/nav/pencil_flag.svg"}
         ]
       },
       {
         toggleLabel: "Writer's Resources",
         submenuItems: [
-          {label: "NaNo Prep", url: "index", segment: null, teaser: "Get ready for the big event", src: "/nav/thought_bubble.svg"},
-          {label: "Pep Talks", url: "index", segment: null, teaser: "Great authors to motivate you", src: "/nav/pompom.svg"},
-          {label: "Offers", url: "index", segment: null, teaser: "Discounts on writer tools", src: "/nav/present.svg"}
+          {label: "NaNo Prep", url: "index", segment: null, teaser: "Get ready for the big event", src: "/images/nav/thought_bubble.svg"},
+          {label: "Pep Talks", url: "index", segment: null, teaser: "Great authors to motivate you", src: "/images/nav/pompom.svg"},
+          {label: "Offers", url: "index", segment: null, teaser: "Discounts on writer tools", src: "/images/nav/present.svg"}
         ]
       }
     ];
