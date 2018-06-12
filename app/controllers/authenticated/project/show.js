7b11231961421c7d2831c9d493ac45248a6d414a
jezra <jezra@jezra.net>
2018-06-12 15:51:37 -0700

    Added 'canUpdateCount' computed property to project/show controller
    Display update button if user canUpdateCount

--- app/controllers/authenticated/project/show.js
+++ app/controllers/authenticated/project/show.js
@@ -1,8 +1,18 @@
 import Controller from '@ember/controller';
+
 import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  currentUser: service(),
+  project: alias('model'),
 
-  project: alias('model')
+  canUpdateCount: computed("currentUser.user.id", "project.user.id", function(){
+    let cuid = this.get("currentUser.user.id");
+    let puid = this.get("project.user.id");
+    console.log(cuid, puid);
+    return cuid == puid;
+  })
 
 });
