83d96dd688fea2ae2bf74aba98ad10a445dcb4e4
jezra <jezra@jezra.net>
2018-07-06 08:52:59 -0700

    Refactored determining if currentUser.user is set

--- app/controllers/authenticated/index.js
+++ app/controllers/authenticated/index.js
@@ -1,16 +1,13 @@
 import Controller from '@ember/controller';
 import { reads }  from '@ember/object/computed';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default Controller.extend({
   currentUser: service(),
   currentUserName: reads('currentUser.user.name'),
   currentUserEmail: reads('currentUser.user.email'),
   currentUserIsNotConfirmed: reads('currentUser.user.isNotConfirmed'),
-  currentUserResolved: computed("currentUser.user", function(){
-    return this.get("currentUser.user");
-  }),
+  
   queryParams: ['addProject'],
   
   addProject: false,
