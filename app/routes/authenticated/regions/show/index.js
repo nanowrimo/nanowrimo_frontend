3fe81fb6ef44560afe168a3ecaa737010a1afdc7
nanodave <dave_beck@nanowrimo.org>
2018-07-16 12:15:33 -0700

    Pre-deploy commit

--- app/routes/authenticated/regions/show/index.js
+++ app/routes/authenticated/regions/show/index.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.get('store').queryRecord('region', { name: params.slug });
+    return this.get('store').queryRecord('region', { slug: params.slug });
   },
 
   setupController(controller, model) {
--- mirage/config.js
+++ mirage/config.js
@@ -47,6 +47,11 @@ export default function() {
   // CRUD
 
   this.resource('regions', { only: ['index'] });
+  this.get('regions/:slug',({regions}, request) => {
+    let slug = request.params.slug;
+    let region = regions.findBy({ slug });
+    return region || new Response(404);
+  });
   //this.resource('locations', { only: ['index'] });
   
   this.resource('challenges', { only: ['index'] });
