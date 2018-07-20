89b0fa975a44e8593bb18e9dfc9886261fd3c550
nanodave <dave_beck@nanowrimo.org>
2018-07-20 13:03:07 -0700

    Using Jez's version of current user service

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -13,20 +13,16 @@ export default Service.extend({
       return this.get('store').queryRecord('user', 
       { current: true, include: 'projects' }).then((user) => {
         this.set('user', user);
+        
         //get the current user's projects 
-        //groupUsers: computed(function() {
-          //return this.get('store').query('group',
-          //{ 
-            i//nclude: 'group-users'
-          //});
-        //});
-        //this.set('user.groupUsers', groupUsers);
         return this.get('store').query('project',
         { 
           filter: { user_id: user.id },
           include: 'genres,challenges,project-challenges,project-sessions'
-        
+          
         });
+        
+        
       });
     } else {
       return resolve();
