b3a99a381200aac0d90de4ec5bfda4b880ac7e82
jezra <jezra@jezra.net>
2018-07-25 13:26:44 -0700

    Send discourse sso info to the API and redirect based on the returned information

--- app/controllers/authenticated/sso.js
+++ app/controllers/authenticated/sso.js
@@ -4,13 +4,17 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   currentUser: service(),
   queryParams: ['sig', 'sso'],
+  
   init() {
     this._super(...arguments);
     //add an observer to the model
     this.addObserver('model', this, 'modelDidChange');
   },
-  //the route model hook has completed and we can now access all service data
+  //the route model hook has completed and we can now access the model
   modelDidChange(){
-    console.log(this.get('currentUser.user.name'));
+    //get the discourse sso url
+    let url = this.get('model.ssoUrl');
+    //redirect the browser to the url
+    window.location.replace(url);
   }
 });
--- app/routes/authenticated/sso.js
+++ app/routes/authenticated/sso.js
@@ -1,11 +1,22 @@
 import Route from '@ember/routing/route';
+import ENV from 'nanowrimo/config/environment';
+import fetch from 'fetch';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+  
   model(params){
-    return {"red":"green"};
-  },
-  setupController(controller, model) {
-    controller.set('model', model);
-
+    //define the API url for processing the SSO request
+    let url = `${ENV.APP.API_HOST}/discourse/sso?sig=${params.sig}&sso=${params.sso}`;
+    //get the session auth token so we can add it to the request header
+    let { auth_token }  = this.get('session.data.authenticated');
+    return fetch(url, {
+      headers: { 'Content-Type': 'application/json', 'Authorization': auth_token}
+    }).then((resp)=>{
+      return resp.json().then((json)=>{
+        return json;
+      });
+    });
   }
 });
