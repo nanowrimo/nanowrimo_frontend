3954866d6d1324bf7677a8e165e71ee0e969f071
jezra <jezra@jezra.net>
2018-10-04 14:58:10 -0700

    Edit project

---
+++ app/components/project-edit-modal.js
@@ -0,0 +1,49 @@
+import Component from '@ember/component';
+import { computed }  from '@ember/object';
+import { inject as service } from '@ember/service';
+import Project from 'nanowrimo/models/project';
+
+export default Component.extend({
+  store: service(),
+  tagName: '',
+
+  open: null,
+  project: null,
+  user: null,
+  project: null,
+
+  optionsForGenres: computed(function() {
+    return this.get('store').findAll('genre');
+  }),
+  optionsForPrivacy: computed(function() {
+    return Project.optionsForPrivacy;
+  }),
+  optionsForStatus: computed(function() {
+    return Project.optionsForStatus;
+  }),
+  optionsForWritingType: computed(function() {
+    return Project.optionsForWritingType;
+  }),
+
+  init() {
+    this._super(...arguments);
+    //what doing?
+  },
+
+  actions: {
+
+    onShow() {
+      console.log('onShow');
+      this.set('open', true);
+    },
+    onHidden() {
+      let callback = this.get('onHidden');
+      if (callback) {
+        console.log('has callback')
+        callback();
+      } else {
+        //this.set('open', null);
+      }
+    }
+  }
+});
--- app/controllers/authenticated/users/show/projects/show.js
+++ app/controllers/authenticated/users/show/projects/show.js
@@ -7,11 +7,38 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   currentUser: service(),
   project: alias('model'),
+  displayEditModal: false,
+  author: null,
+  
+  canEdit: computed('author','currentUser.user', function(){
+    let a = this.get('author');
+    let cu = this.get('currentUser.user');
+    return a==cu;
+  }),
 
+  init(){
+    this._super(...arguments);
+  },
+
+  actions: {
+    editProject() {
+      if(this.get('canEdit')){
+        console.log('editproject');
+        this.set('displayEditModal', true);
+      }
+    },
+
+    afterEditProjectModalClose(){
+      console.log('after edit project modal close');
+      this.set('displayEditModal', false);
+    }
+  }
+
+  /* TODO: remove canUpdateCount
   canUpdateCount: computed("currentUser.user.id", "project.user.id", function(){
     let cuid = this.get("currentUser.user.id");
     let puid = this.get("project.user.id");
     return cuid == puid;
   })
-
+  */
 });
