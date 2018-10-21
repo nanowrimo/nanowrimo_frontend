cd72358c84dc7e4aa1298e9497983b83476a17a5
nanojezra <jezra@nanowrimo.org>
2018-10-21 14:21:20 -0700

    initial per/project dropdown for editing

--- app/components/project-listing.js
+++ app/components/project-listing.js
@@ -1,18 +1,42 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  currentUser: service(),
+  editProject: false,
+  
   queryParams: ['editCover', 'editCoverTab'],
   
   editCover: null,
   editCoverTab: null,
   
+  canEdit: computed('project', function(){
+    return this.get('currentUser.user') === this.get('author');
+  }),
+  
   actions: {
+    doNothing() {
+      //no really, do nothing 
+    },
+    
     afterCoverModalClose() {
       this.set('editCover', null);
       this.set('editCoverTab', null);
     },
 
     openCoverModal() {
+      this.set('editCover', true);
+      this.set('editCoverTab', null);
+    },
+    
+    editOverview() {
+      this.set('editProject', true);
+    },
+    editDetails() {
+      this.set('editProject', true);
+    },
+    updateCover() {
       this.set('editCover', true);
       this.set('editCoverTab', null);
     },
