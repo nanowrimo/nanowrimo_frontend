887c227e6fbe7d5a0b5362c07521012a519a8bbd
nanojezra <jezra@nanowrimo.org>
2018-10-25 15:02:57 -0700

    Styling improvement, beginning of delete/edit actions

--- app/components/project-challenge-listing.js
+++ app/components/project-challenge-listing.js
@@ -1,7 +1,21 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  currentUser: service(),
+  editProjectChallenge: false,
+  showConfirmDelete: false,
+  
+  
+  
+  
+  
+  canEdit: computed('project', function(){
+    let currentUser = this.get('currentUser.user');
+    let author = this.get('author');
+    return currentUser === author;
+  }),
   
   svg: computed('projectChallenge', function() {
     let str = (this.get('projectChallenge.unitType') === 0) ? 'writing' : 'editing';
@@ -9,12 +23,33 @@ export default Component.extend({
   }),
   
   
+  init() {
+    this._super(...arguments);
+    this.set('deleteConfirmationYesText', 'Yes');
+    this.set('deleteConfirmationNoText', 'No'); 
+    this.set('deleteConfirmationTitleText', 'Confirm');
+    this.set('deleteConfirmationBodyText', 'Do you really wish to delete this goal?');
+  },
+  
+  
+  
   actions: {
-   deleteConfirmationYes(){
-      
+    editProjectChallenge(){
+       this.set('editProjectChallenge', true);
+    },
+    confirmDelete(){
+      //show the delete dialog
+      this.set('showConfirmDelete', true);
+    },
+    
+    deleteConfirmationYes(){
+      //TODO: delete this projectChallenge
+      //close the modal
+      this.set('showConfirmDelete', false);
     },
     deleteConfirmationNo(){
-      
+      //close the modal
+      this.set('showConfirmDelete', false);
     }
   }
 });
--- app/routes/authenticated/users/show/projects/show/goals.js
+++ app/routes/authenticated/users/show/projects/show/goals.js
@@ -4,6 +4,11 @@ export default Route.extend({
   model() {
     let project = this.modelFor('authenticated.users.show.projects.show');
     return this.get('store').query('projectChallenge', { filter: {project_id: project.id}, include: 'challenge,project-sessions'});
+  },
+  
+  setupController(controller, model) { // eslint-disable-line no-unused-vars
+    this._super(...arguments);
+    controller.set('author', this.modelFor('authenticated.users.show') );
   }
 
 });
