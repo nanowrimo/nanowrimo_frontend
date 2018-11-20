3183baa8c06fcfa2fa98f1cf3b550434bb76f2e3
nanojezra <jezra@nanowrimo.org>
2018-11-20 13:50:40 -0800

    Init how I feel goal dashboard widget

--- app/components/stats-how-i-feel.js
+++ app/components/stats-how-i-feel.js
@@ -1,4 +1,24 @@
 import ChartBaseComponent from 'nanowrimo/components/chart-base-component';
+import {computed} from '@ember/object';
 
 export default ChartBaseComponent.extend({
+
+  feelingString: computed('averageFeeling', function(){
+    let strings = ['upset','stressed','just ok', 'pretty good', 'great'];
+    return strings[this._feeling()-1 ];
+  }),
+  
+  feelingIntroString: computed('averageFeeling', function(){
+    let strings = ['Oh no','Uh oh','Hmmm', 'Nice', 'Nice'];
+    return strings[this._feeling()-1 ];
+  }),
+  
+  feelingImg: computed('averageFeeling', function(){
+    return `/images/cards/smiley${this._feeling()}.svg`;
+    
+  }),
+  
+  _feeling: function(){
+    return parseInt(this.get('averageFeeling'));
+  }
 });
