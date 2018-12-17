2ba9007303296c26e8fba857fbee4eadaebcb732
nanojezra <jezra@nanowrimo.org>
2018-12-17 11:23:27 -0800

    Render Pages if pages exist

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -1,5 +1,19 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
+import ENV from 'nanowrimo/config/environment';
 
 export default Route.extend({
-  templateName: '404'
+  templateName: '404',
+  model(params) {
+    if (params.path) {
+      let endpoint =  `${ENV.APP.API_HOST}/pages/${params.path}`;
+      console.log(endpoint);
+      return fetch(endpoint).then((data)=>{
+        return data.json().then((json)=>{
+          console.log(json);
+          return json;
+        });
+      });
+    }
+  }
 });
