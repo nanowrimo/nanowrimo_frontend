1d139e1f9dd4abd1d377f3d5e9e21bae73e6e7a2
nanojezra <jezra@nanowrimo.org>
2019-01-17 16:03:26 -0800

    Fixed method to pass test

--- app/helpers/newline-to-br.js
+++ app/helpers/newline-to-br.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-export function newlineToBr([text, ...rest]) {
+export function newlineToBr([text]) {
   //strip html from the text
   let processedText = text.replace(/\r\n/g, "\n");
   processedText = text.replace(/\r/g, "\n");
