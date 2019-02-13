0573c8d3219d90fff8eddf5698a67c5276ed1fef
nanodave <dave_beck@nanowrimo.org>
2019-02-13 11:07:45 -0800

    Forcing deploy

--- config/deploy.js
+++ config/deploy.js
@@ -6,6 +6,7 @@ module.exports = function(deployTarget) {
   if (deployTarget === 'staging') {
     assetsBucket = process.env.ASSETS_STAGING_BUCKET;
     indexBucket = process.env.INDEX_STAGING_BUCKET;
+    
   }
   if (deployTarget === 'production') {
     assetsBucket = process.env.ASSETS_PRODUCTION_BUCKET;
