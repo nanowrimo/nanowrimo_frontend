787147c5cbc7f660b5bc8090ab9921be95d1103b
nanodave <dave_beck@nanowrimo.org>
2019-02-14 09:57:56 -0800

    Changing staging build environment

--- config/environment.js
+++ config/environment.js
@@ -108,6 +108,12 @@ module.exports = function(environment) {
     ENV.APP.MODAL_BACKGROUND_TRANSITION_MS = 0;
     ENV.APP.MODAL_TRANSITION_MS = 0;
   }
+  if (environment === 'staging') {
+    ENV.APP.API_HOST = 'https://staging.api.nanowrimo.org';
+    ENV.torii.providers['facebook-connect'].appId = '2019466444992364';
+    ENV.torii.providers['custom-google'].apiKey = '566453198538-khkvh94le8q9a2j0jmrokg8faajotr38.apps.googleusercontent.com';
+    ENV.torii.providers['custom-google'].redirectUri = 'https://preview.nanowrimo.org/oauth2callback';
+  }
   if (environment === 'production') {
     ENV.torii.providers['facebook-connect'].appId = '2019466444992364';
     ENV.torii.providers['custom-google'].apiKey = '566453198538-khkvh94le8q9a2j0jmrokg8faajotr38.apps.googleusercontent.com';
