04fa3e0bfc178ba4813d56e1740afbdfb25a110f
nanojezra <jezra@nanowrimo.org>
2019-02-15 11:41:38 -0800

    Make settings/privacy form more user friendly
    disable submit until form values have changed
    reset selects to initial value when form is reset

--- app/components/form-for--save-cancel.js
+++ app/components/form-for--save-cancel.js
@@ -1,11 +1,18 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   changeset: null,
   
+  isPristine: computed("changeset.isPristine", function() {
+    let pristine = this.get('changeset').get('isPristine');
+    return pristine;
+  }),
+  
   actions: {
     rollbackChanges: function() {
       //rollback the changeset
+      let cs = this.get('changeset');
       this.get('changeset').rollback();
     }
   }
--- app/components/form-for--select.js
+++ app/components/form-for--select.js
@@ -3,6 +3,8 @@ import { isArray } from '@ember/array';
 import { assert } from '@ember/debug';
 import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
+import { next } from '@ember/runloop';
+
 
 export default Component.extend({
   changeset: null,
@@ -10,17 +12,64 @@ export default Component.extend({
   label: '',
   options: null,
   property: null,
-
+  value: null,
+  initialValue: null,
+  hasChange: false,
+  showSelect: true,
+  
   name: reads('property'),
-
-  normalizedOptions: computed('options.[]', function() {
+  
+  //track some changes
+  didChange: computed('changeset.changes.[]', function(){
+    //the changeset changed, is it now pristine?
+    let isPristine = this.get('changeset.isPristine');
+    
+    if (isPristine && this.get('hasChange') ) {
+      this.set('value', this.get('initialValue') );
+      this._resetShowSelect();
+    }
+    
+    let cc = this.get('changeset.changes');
+    cc.forEach((c)=>{
+      if (c.key == this.get('property')) {
+        this.set('hasChange', true);
+      }
+    });
+    
+    return "";
+  }),
+  
+  normalizedOptions: computed('options.[]','viewReset', function() {
     return this.get('options').map(function(option) {
       return (typeof option === 'string') ? { name: option, value: option } : option;
     });
   }),
-
+  
+  valueAsString: computed('value', function() {
+    let value = this.get('value').toString();
+    return value;
+  }),
+  
   init() {
     this._super(...arguments);
     assert('Must pass options array into {{form-for--select}}', isArray(this.get('options')));
+    //set the initial value that we need to track
+    let v = this.get(`changeset.${this.get('property')}`);
+    this.set('value', v);
+    this.set('initialValue', v);
+  },
+  
+  actions: {
+    valueChanged: function(value){
+      this.set(`changeset.${this.get('property')}`, value);
+      this.set('value', value);
+    }
+  },
+  _resetShowSelect: function() {
+    this.set('showSelect', null);
+    //wait a bit and set the pc to the proper value
+    next(()=>{
+      this.set('showSelect', true);
+    });
   }
 });
