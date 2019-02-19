b48522316b648bf0d62e456eda68ee940a2eae13
nanodave <dave_beck@nanowrimo.org>
2019-02-19 12:54:29 -0800

    Tweak to test deploy

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function(environment, isStaging = false) {
   let ENV = {
     modulePrefix: 'nanowrimo',
     environment,
@@ -109,12 +109,17 @@ module.exports = function(environment) {
     ENV.APP.MODAL_TRANSITION_MS = 0;
   }
   if (environment === 'staging') {
-    ENV.APP.API_HOST = 'https://staging.api.nanowrimo.org';
+    if isStaging {
+      ENV.APP.API_HOST = 'https://staging.api.nanowrimo.org';
+    }
     ENV.torii.providers['facebook-connect'].appId = '2019466444992364';
     ENV.torii.providers['custom-google'].apiKey = '566453198538-khkvh94le8q9a2j0jmrokg8faajotr38.apps.googleusercontent.com';
     ENV.torii.providers['custom-google'].redirectUri = 'https://preview.nanowrimo.org/oauth2callback';
   }
   if (environment === 'production') {
+    if isStaging {
+      ENV.APP.API_HOST = 'https://staging.api.nanowrimo.org';
+    }
     ENV.torii.providers['facebook-connect'].appId = '2019466444992364';
     ENV.torii.providers['custom-google'].apiKey = '566453198538-khkvh94le8q9a2j0jmrokg8faajotr38.apps.googleusercontent.com';
     ENV.torii.providers['custom-google'].redirectUri = 'https://preview.nanowrimo.org/oauth2callback';
