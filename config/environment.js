590f086ec4b6dc1c1e10d49e0e50cf36055d76d0
nanojezra <jezra@nanowrimo.org>
2019-02-19 15:43:53 -0800

    Changed the environment to handle a staging TARGET

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      API_HOST: 'https://api.nanowrimo.org',
+      API_HOST: (process.env.TARGET==='staging'): 'https://staging.api.nanowrimo.org' : 'https://api.nanowrimo.org',
       DEBOUNCE_MS: 250,
       SOCIAL_SERVICES: ['facebook', 'twitter', 'instagram', 'medium', 'tumblr', 'youtube'],
       MODAL_BACKGROUND_TRANSITION_MS: 150,
