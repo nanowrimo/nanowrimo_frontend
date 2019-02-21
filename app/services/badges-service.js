cfa5a4ca03d6864873b94ee1d2fd81fca2838967
nanodave <dave_beck@nanowrimo.org>
2019-02-21 10:28:31 -0800

    reverting one commit back

--- app/services/badges-service.js
+++ app/services/badges-service.js
@@ -15,15 +15,13 @@ export default Service.extend({
   checkForUpdates() {
     let t = this;
     let u = this.get('currentUser.user');
-    if (u) {
-      this.store.query('user-badge', {
-        filter: {
-          user_id: u.get('id')
-        }
-      }).then(function() {
-        t.incrementRecomputeBadges();
-      });
-    }
+    this.store.query('user-badge', {
+      filter: {
+        user_id: u.get('id')
+      }
+    }).then(function() {
+      t.incrementRecomputeBadges();
+    });
     debounce(this, this.checkForUpdates, 15000, false);
   },
   
