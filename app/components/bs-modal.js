91bb35219c5e43b737d5f09c40727976e73fbea9
nanojezra <jezra@nanowrimo.org>
2019-03-20 13:30:07 -0700

    Don't close modals when clicking outside of modal

--- app/components/bs-modal.js
+++ app/components/bs-modal.js
@@ -3,5 +3,6 @@ import ENV from 'nanowrimo/config/environment';
 
 export default BSModalComponent.extend({
   backdropTransitionDuration: ENV.APP.MODAL_BACKGROUND_TRANSITION_MS,
-  transitionDuration: ENV.APP.MODAL_TRANSITION_MS
+  transitionDuration: ENV.APP.MODAL_TRANSITION_MS,
+  backdropClose: false
 });
