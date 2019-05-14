c2cc7b1cc739e20d813e643ff0d6c278a1ffca00
nanodave <dave_beck@nanowrimo.org>
2019-05-14 09:52:52 -0700

    Commit prior to pull

--- app/controllers/authenticated/search.js
+++ app/controllers/authenticated/search.js
@@ -1,10 +1,15 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 export default Controller.extend({
   currentUser: service(),
   router: service(),
   queryParams: ['input'],
   input: null,
+  resultString: computed("model.data", function() {
+    return JSON.stringify(this.model.data);
+  }),
+  
   actions: {
   }
 });
