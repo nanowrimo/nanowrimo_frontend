d1e822707c3b7827ad191bb223f3931973d11478
nanodave <dave_beck@nanowrimo.org>
2019-06-13 13:15:30 -0700

    Committing to merge Jez's code

---
+++ app/components/project-flippy-details.js
@@ -0,0 +1,11 @@
+import NwFlippyCard from 'nanowrimo/components/nw-flippy-card';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/template';
+
+export default NwFlippyCard.extend({
+  project: null,
+  /*safeSrc: computed('post.attributes.card-image', function() {
+    return new htmlSafe( "background-image: url(" + this.get('post.attributes.card-image') + ")" );
+  })*/
+    
+});
