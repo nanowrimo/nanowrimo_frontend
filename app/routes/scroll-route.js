2a60320089d37f3fe58710647b87e801b965d2a0
nanojezra <jezra@nanowrimo.org>
2019-07-08 16:14:21 -0700

    Added templates that have been updated to use query params to maintain scroll\

--- app/routes/scroll-route.js
+++ app/routes/scroll-route.js
@@ -4,3 +4,12 @@ import routeScrollHandler from 'nanowrimo/mixins/route-scroll-handler';
 export default Route.extend(routeScrollHandler, {
 
 });
+
+/* This is the base route for handling "scroll to top"
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
