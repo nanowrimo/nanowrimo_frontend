440f46c7384f0e363997542c087238b69950f83f
nanojezra <jezra@nanowrimo.org>
2019-07-31 13:57:38 -0700

    Removing form-for from user edit settings

--- app/controllers/authenticated/settings/account.js
+++ app/controllers/authenticated/settings/account.js
@@ -2,14 +2,32 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import TimeZones from 'nanowrimo/lib/time-zones';
+import $ from 'jquery';
 
 export default Controller.extend({
   error: null, // String OR object
+  store: service(),
   currentUser: service(),
-  newEmail: null,
-  newPassword: null,
+  formID: null,
+  formChangeCount: 0,
+  nameError: null,
+  emailError: null,
+  passwordError: null,
+  currentPasswordError: null,
   showPasswordConfirm: false,
   confirmationPasswordError: false,
+  
+  
+  init(){
+    this._super(...arguments);
+    this.set('user', this.get("currentUser.user"));
+    this.set('formID', "account-settings");
+  },
+  
+  timeZoneOptions: computed(function() {
+    return TimeZones;
+  }),
+  
   // For displaying response messages
   _formResponseMessage: null,
   formResponseMessage: computed('_formResponseMessage',function() {
@@ -17,8 +35,8 @@ export default Controller.extend({
   }),
   
   // Hides or shows the password fields depending on whether the user has a password
-  registrationPath: computed('currentUser.user.registrationPath',function() {
-    return this.get('currentUser.user.registrationPath');
+  registrationPath: computed('user.registrationPath',function() {
+    return this.get('user.registrationPath');
   }),
   // Hides or shows the password fields depending on whether the user has a password
   hasPassword: computed('registrationPath',function() {
@@ -34,35 +52,122 @@ export default Controller.extend({
     let error = this.get('error');
     return (typeof error === 'object') ? error : null;
   }),
-
   
-  timeZoneOptions: computed(function() {
-    //let c = this.get('currentUser.user.projects');
-    return TimeZones;
+  hasChangedValues: computed('formChangeCount', function(){
+    //get the form
+    let user = this.get('user');
+    let id = this.get('formID')
+    let form = $(`#${id}`)[0];
+    if (form.username.value != user.name) return true;
+    if (form.email.value != user.email) return true;
+    if(form.password.value) {
+      if (form.currentPassword.value) return true;
+    }
   }),
   
   actions: {
-    newPasswordChanged(val) {
-      if (val) {
-        this.set("showPasswordConfirm", val.length > 0);
-      }
+    nameChanged() {
+      this.incrementProperty('formChangeCount');
     },
+    
+    emailChanged() {
+      this.incrementProperty('formChangeCount');
+    },
+    
+    timeZoneChanged() {
+      this.incrementProperty('formChangeCount');
+    },
+    
+    newPasswordChanged(val) {
+      this.set("showPasswordConfirm", val.length > 0);
+      this.incrementProperty('formChangeCount');
+    },
+    currentPasswordChanged(val) {
+      this.incrementProperty('formChangeCount');
+    },
+    
     afterError(error) {
       this.set('error', error);
       if (error.errors[0].title=="BAD-AUTH") {
         this.set('confirmationPasswordError', true);
       }
     },
+    
+    submit() {
+      //get the form
+      let id = this.get('formID')
+      let form = $(`#${id}`)[0];
+      //attempt to update current user,
+      let user = this.get('user');
+      let name = form.username.value;
+      let email = form.email.value;
+      let password = form.password.value;
+      if(password){
+        let currentPassword = form.currentPassword.value;
+      }
+      let timezone = form.timezone.value;
+      
+      
+      let hasError = false;
+      //simple validation
+      if (name.length===0) {
+        hasError = true;
+        this.set('nameError', "Username cannot be blank");
+      }
+      if (email.length===0) {
+        hasError = true;
+        this.set('emailError', "Email cannot be blank");
+      }
+      if (password.length>0) {
+        if (currentPassword.length==0) {
+          hasError = true;
+          this.set("currentPasswordError", "Current Password is required");
+        }
+      }
+      
+      //if there are no errors...
+      if(!hasError) {
+        //update the user and submit
+        user.set('name', name);
+        user.set('email', email);
+        if(password){
+          user.set('newPassword', password);
+          user.set('currentPassword', currentPassword);
+        }
+        user.set('timeZone', timezone);
+        
+        user.save().then(()=>{
+          //success
+        }).catch((err)=>{
+          console.log(err);
+        });
+      }
+    },
+    
     afterSubmit() {
       this.set('confirmationPasswordError', false);
-      let u = this.get('currentUser.user');
+      let u = this.get('user');
       let ne = this.get('newEmail');
       let r = "Your changes have been saved.";
       if (u.email != ne && ne != null) {
         r = r + " We've sent a confirmation email to " + ne + ". Please check your email to complete the change of email address.";
       }
       this.set('_formResponseMessage',r);
+    },
+    
+    reset() {
+      //get the form
+      let id = this.get('formID')
+      let form = $(`#${id}`)[0];
+      //update values
+      form.username.value = this.get('user.name'); 
+      form.email.value = this.get('user.email'); 
+      form.password.value = '';
+      form.timezone.value = this.get('user.timeZone');
+      //don't show the password confirm
+      this.set("showPasswordConfirm", false);
+      //consider a reset a form change 
+       this.incrementProperty('formChangeCount');
     }
   }
-  
 });
