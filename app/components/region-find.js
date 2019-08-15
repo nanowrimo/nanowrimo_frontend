3df98bebed21383a033ffb39dcfd6badb64f7c75
nanojezra <jezra@nanowrimo.org>
2019-08-15 12:18:46 -0700

    Don't wait for region-find model to resolve before displaying UI to the user

--- app/components/region-find.js
+++ app/components/region-find.js
@@ -154,9 +154,12 @@ export default Component.extend({
   },
 
   sortedRegions: computed('currentUser.user.homeRegion','regions','joinedRegionIds','searchString','sortOption', function() {
-    //var date = new Date();
-    //var timestamp = date.getTime();
+    //get the regions 
     let r = this.get('regions');
+    //if there are no regions, return an empty array
+    if (!r) {
+      return [];
+    }
     let joinedRegionIds = this.get('joinedRegionIds');
     let s = this.get('sortOption');
     let listLength = 10;
