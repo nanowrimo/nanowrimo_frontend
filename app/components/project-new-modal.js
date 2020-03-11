84d1cc4a2d1cf97a514634cf04c93bdb6502e7ba
nanodave <dave_beck@nanowrimo.org>
2020-03-11 13:21:49 -0700

    Selecting event on new project creation

--- app/components/project-new-modal.js
+++ app/components/project-new-modal.js
@@ -3,11 +3,13 @@ import { assert } from '@ember/debug';
 import { sort } from '@ember/object/computed';
 import { computed, observer }  from '@ember/object';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 import Project from 'nanowrimo/models/project';
 import Changeset from 'ember-changeset';
 
 export default Component.extend({
   store: service(),
+  currentUser: service(),
 
   tagName: 'span',
   getChallengeOptions: false,
@@ -39,10 +41,12 @@ export default Component.extend({
     let challenges = this.get('optionsForChallenges');
     if (challenges.length > 0) {
       let latest = challenges[0];
-      let d = new Date();
-      if (latest.endsAt > d) {
+      //let d = new Date();
+      //if (latest.endsAt > d) {
+      let now = moment();
+      if (latest.endsAt >= now.format("YYYY-MM-DD")) {
 
-
+        this.set('associateWithChallenge',false);
         this.send('clickedAssociateCheckbox');
         
       } else {
@@ -107,6 +111,71 @@ export default Component.extend({
       this.set('projectChallengeChangeset', new Changeset(newProjectChallenge) );
     }
   },
+  
+  // Returns true is it's an event and the user hasn't created a project
+  /*checkEventBox() {
+    // Set a variable for whether the challenge exists
+    let d = false;
+    // Set a local variable for the store
+    let store = this.get('store');
+    // Set a local variable for all challenges in the store
+    let cs = store.peekAll('challenge');
+    // Set a local variable for the correct challenge
+    let newc = null;
+    // Loop through the challenges to find the latest event
+    cs.forEach(function(c) {
+      // If this is an event
+      if ((c.eventType==0)||(c.eventType==1)) {
+        // If the challenge is newer than ones already found
+        if ((newc===null)||(newc.endsAt<c.endsAt)) {
+          // Set the challenge variable to this challenge
+          newc = c;
+        }
+      }
+    });
+    // If the challenge has been found...
+    if (newc) {
+      // Get the current user
+      let cu = this.get('currentUser.user');
+      if (cu) {
+        if (cu.currentDateInDateRange(newc.prepStartsAt,newc.endsAt)) {
+          if ((newc.eventType==0)||(newc.eventType==1)) {
+            d = true;
+            // Get all project_challenges
+            let pcs = store.peekAll('project-challenge');
+            // Loop through them
+            pcs.forEach(function(pc) {
+              // If this project challenge is for the latest event...
+              if (newc.id==pc.challenge_id) {
+                // Find the associated project
+                let p = store.peekRecord('project',pc.project_id);
+                // If the project is found
+                if (p) {
+                  // If the current user is the author
+                  if (p.user_id==cu.id) {
+                    d = false;
+                  }
+                }
+              }
+            });
+          }
+        }
+      }
+    }
+    //alert(d);
+    //this.set('associateWithChallenge',d);
+    if (d) {
+      this.set('associateWithChallenge',d);
+      //get the challenge
+      alert(this.get('optionsForChallenges.firstObject'));
+      if (this.get("associatedChallenge") === null) {
+        //set the challenge id to the id of the first object in options for Challenges
+        this.set('associatedChallenge', this.get('optionsForChallenges.firstObject'));
+      }
+      this.set('challenge', this.get("associatedChallenge"));
+    }
+  },*/
+  
 
   actions: {
     associateChallengeSelect(challengeID) {
