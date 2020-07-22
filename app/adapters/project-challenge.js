f2492a374a646050106f2b78c3177e66dd175f49
nanojezra <jezra@nanowrimo.org>
2020-07-22 16:51:04 -0700

    modified adapter to import get

--- app/adapters/project-challenge.js
+++ app/adapters/project-challenge.js
@@ -1,9 +1,10 @@
 import ApplicationAdapter from './application';
+import { get } from '@ember/object';
 
 export default ApplicationAdapter.extend({
   urlForFindRecord(id, modelName, snapshot) {
     // is there a need to have the API recompute the project-challenge?
-    let doRecompute = Ember.get(snapshot, 'adapterOptions.query.recompute');
+    let doRecompute = get(snapshot, 'adapterOptions.query.recompute');
     if (doRecompute) {
        return this._super(...arguments)+"?recompute=true";
     }
