a254d50d708a1cb586f0103146d508120e63bb2b
nanodave <dave_beck@nanowrimo.org>
2020-08-06 17:51:37 -0700

    Redesign of update modal ref #368

--- app/components/main-nav-session-logger.js
+++ app/components/main-nav-session-logger.js
@@ -24,7 +24,7 @@ export default Component.extend({
   writingMethods: null,
   referenceTimer: null,
   referenceStopwatch: null,
-  _projectAdditionalInfoShow: false,
+  _projectAdditionalInfoShow: true,
   projectSelected: 0,
   open: true,
   
