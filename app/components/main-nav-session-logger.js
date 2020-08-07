3778a53b8b7d48479327d308ca1b81b0635ba6a9
nanodave <dave_beck@nanowrimo.org>
2020-08-07 15:15:12 -0700

    Added drop shadow to book cover ref #368

--- app/components/main-nav-session-logger.js
+++ app/components/main-nav-session-logger.js
@@ -24,7 +24,7 @@ export default Component.extend({
   writingMethods: null,
   referenceTimer: null,
   referenceStopwatch: null,
-  _projectAdditionalInfoShow: true,
+  _projectAdditionalInfoShow: false,
   projectSelected: 0,
   open: true,
   
