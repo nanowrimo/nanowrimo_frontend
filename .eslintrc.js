3a3e79f7d6553f421352e942ca00123f8b352d38
nanojezra <jezra@nanowrimo.org>
2020-08-25 16:04:01 -0700

    v3.12.0...v3.20.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,22 @@
+'use strict';
+
 module.exports = {
   globals: {
     server: true,
   },
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
+/*<<<<<<< HEAD
     ecmaVersion: 2017,
     sourceType: 'module'
+=======*/
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+//>>>>>>> fba111b... v3.12.0...v3.20.0
   },
   plugins: [
     'ember'
@@ -17,11 +28,15 @@ module.exports = {
   env: {
     browser: true
   },
+<<<<<<< HEAD
   rules: {
     "ember/no-observers":1,
     "ember/no-new-mixins":1,
     "ember/require-return-from-computed":1
   },
+=======
+  rules: {},
+>>>>>>> fba111b... v3.12.0...v3.20.0
   overrides: [
     // node files
     {
@@ -39,6 +54,16 @@ module.exports = {
       env: {
         browser: false,
         node: true
+<<<<<<< HEAD
+=======
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
+>>>>>>> fba111b... v3.12.0...v3.20.0
       }
     }
   ]
--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import ENV from 'nanowrimo/config/environment';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-
+/*
 export default JSONAPIAdapter.extend({
   session: service(),
 
@@ -13,3 +13,15 @@ export default JSONAPIAdapter.extend({
     return auth_token ? { 'Authorization': auth_token } : {};
   })
 });
+*/
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  // Application specific overrides go here
+
+  @service session;
+  host= ENV.APP.API_HOST;
+
+  get headers() {
+    let { auth_token } = this.session.data.authenticated;
+    return auth_token ? { 'Authorization': auth_token } : {};
+  }
+}
