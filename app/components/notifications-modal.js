892316dac602b187873fcf6062f005739cb3a53a
nanodave <dave_beck@nanowrimo.org>
2020-09-04 10:40:47 -0700

    Finished modal creation for notifications ref #415

--- app/components/notifications-modal.js
+++ app/components/notifications-modal.js
@@ -5,6 +5,8 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   store: service(),
   notificationsService: service(),
+  router: service(),
+  
   allNotifications: null,
   newNotificationsCount: null,
   classNames: ['nw-noti-modal-container'],
@@ -14,11 +16,24 @@ export default Component.extend({
   actions: {
     onShow() {
       this.set('open',true);
+      this.get('notificationsService').notificationsViewed();
     },
     onHidden() {
       this.set('open',false);
     },
     cancel(){
+      let cfa = this.get('closeFormAction');
+      cfa();
+    },
+    openPreferences() {
+      let r = this.get('router');
+      r.transitionTo('authenticated.settings.preferences');
+      let cfa = this.get('closeFormAction');
+      cfa();
+    },
+    openNotifications() {
+      let r = this.get('router');
+      r.transitionTo('authenticated.notifications');
       let cfa = this.get('closeFormAction');
       cfa();
     },
