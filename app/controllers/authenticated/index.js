d1629643b67a1c7621de7e02bfba7d0220e766f0
fatimamah <fam76@cornell.edu>
2020-09-16 16:28:47 -0500

    Add alt text for  images on homepage

--- app/controllers/authenticated/index.js
+++ app/controllers/authenticated/index.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { reads }  from '@ember/object/computed';
+import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
@@ -8,11 +8,11 @@ export default Controller.extend({
   store: service(),
   currentUser: service(),
   router: service(),
-  
+
   currentUserName: reads('currentUser.user.name'),
   currentUserEmail: reads('currentUser.user.email'),
   currentUserIsNotConfirmed: reads('currentUser.user.isNotConfirmed'),
-  currentUserHasProject: computed('currentUser.user.projects.{[],@each.primary}', function() {
+  currentUserHasProject: computed('currentUser.user.projects.{[],@each.primary}', function () {
     let cu = this.get('currentUser.user');
     if (cu) {
       return cu.persistedProjects.length > 0;
@@ -20,13 +20,13 @@ export default Controller.extend({
       return false;
     }
   }),
-  primaryProject: computed("currentUser.user.primaryProject", function(){
+  primaryProject: computed("currentUser.user.primaryProject", function () {
     let p = this.get('currentUser.user.primaryProject');
     return p;
   }),
-  
+
   // Returns true is it's camp and the user hasn't created a project
-  displayCampBanner: computed('currentUser.user.projects.[]','primaryProject.currentProjectChallenge.count',function() {
+  displayCampBanner: computed('currentUser.user.projects.[]', 'primaryProject.currentProjectChallenge.count', function () {
     let d = false;
     // Set a local variable for the store
     let store = this.get('store');
@@ -35,11 +35,11 @@ export default Controller.extend({
     // Set a local variable for the correct challenge
     let newc = null;
     // Loop through the challenges to find the latest event
-    cs.forEach(function(c) {
+    cs.forEach(function (c) {
       // If this is an event
-      if ((c.eventType==0)||(c.eventType==1)) {
+      if ((c.eventType == 0) || (c.eventType == 1)) {
         // If the challenge is newer than ones already found
-        if ((newc===null)||(newc.endsAt<c.endsAt)) {
+        if ((newc === null) || (newc.endsAt < c.endsAt)) {
           // Set the challenge variable to this challenge
           newc = c;
         }
@@ -50,21 +50,21 @@ export default Controller.extend({
       // Get the current user
       let cu = this.get('currentUser.user');
       if (cu) {
-        if (cu.currentDateInDateRange(newc.prepStartsAt,newc.endsAt)) {
-          if (newc.eventType==1) {
+        if (cu.currentDateInDateRange(newc.prepStartsAt, newc.endsAt)) {
+          if (newc.eventType == 1) {
             d = true;
             // Get all project_challenges
             let pcs = store.peekAll('project-challenge');
             // Loop through them
-            pcs.forEach(function(pc) {
+            pcs.forEach(function (pc) {
               // If this project challenge is for the latest event...
-              if (newc.id==pc.challenge_id) {
+              if (newc.id == pc.challenge_id) {
                 // Find the associated project
-                let p = store.peekRecord('project',pc.project_id);
+                let p = store.peekRecord('project', pc.project_id);
                 // If the project is found
                 if (p) {
                   // If the current user is the author
-                  if (p.user_id==cu.id) {
+                  if (p.user_id == cu.id) {
                     d = false;
                   }
                 }
@@ -77,13 +77,13 @@ export default Controller.extend({
     // Return if they won or not
     return d;
   }),
-    
+
   // Returns true is it's camp and the user hasn't created a project
-  displayNanoBanner: computed('currentUser.user.projects.[]','primaryProject.currentProjectChallenge.count',function() {
+  displayNanoBanner: computed('currentUser.user.projects.[]', 'primaryProject.currentProjectChallenge.count', function () {
     let ps = this.get('currentUser.user.projects');
     let c = this.get('primaryProject.currentProjectChallenge.count');
     let d = false;
-    if (ps===c) {
+    if (ps === c) {
       d = false;
     }
     // Set a local variable for the store
@@ -93,11 +93,11 @@ export default Controller.extend({
     // Set a local variable for the correct challenge
     let newc = null;
     // Loop through the challenges to find the latest event
-    cs.forEach(function(c) {
+    cs.forEach(function (c) {
       // If this is an event
-      if ((c.eventType==0)||(c.eventType==1)) {
+      if ((c.eventType == 0) || (c.eventType == 1)) {
         // If the challenge is newer than ones already found
-        if ((newc===null)||(newc.endsAt<c.endsAt)) {
+        if ((newc === null) || (newc.endsAt < c.endsAt)) {
           // Set the challenge variable to this challenge
           newc = c;
         }
@@ -108,21 +108,21 @@ export default Controller.extend({
       // Get the current user
       let cu = this.get('currentUser.user');
       if (cu) {
-        if (cu.currentDateInDateRange(newc.prepStartsAt,newc.endsAt)) {
-          if (newc.eventType==0) {
+        if (cu.currentDateInDateRange(newc.prepStartsAt, newc.endsAt)) {
+          if (newc.eventType == 0) {
             d = true;
             // Get all project_challenges
             let pcs = store.peekAll('project-challenge');
             // Loop through them
-            pcs.forEach(function(pc) {
+            pcs.forEach(function (pc) {
               // If this project challenge is for the latest event...
-              if (newc.id==pc.challenge_id) {
+              if (newc.id == pc.challenge_id) {
                 // Find the associated project
-                let p = store.peekRecord('project',pc.project_id);
+                let p = store.peekRecord('project', pc.project_id);
                 // If the project is found
                 if (p) {
                   // If the current user is the author
-                  if (p.user_id==cu.id) {
+                  if (p.user_id == cu.id) {
                     d = false;
                   }
                 }
@@ -135,9 +135,9 @@ export default Controller.extend({
     // Return if they won or not
     return d;
   }),
-    
+
   // Returns the current event name if it's happening, false if not
-  currentEventWelcome: computed('currentUser.user.projects.[]','primaryProject.currentProjectChallenge.count',function() {
+  currentEventWelcome: computed('currentUser.user.projects.[]', 'primaryProject.currentProjectChallenge.count', function () {
     let str = "Welcome";
     // Set a local variable for the store
     let store = this.get('store');
@@ -146,11 +146,11 @@ export default Controller.extend({
     // Set a local variable for the correct challenge
     let newc = null;
     // Loop through the challenges to find the latest event
-    cs.forEach(function(c) {
+    cs.forEach(function (c) {
       // If this is an event
-      if ((c.eventType==0)||(c.eventType==1)) {
+      if ((c.eventType == 0) || (c.eventType == 1)) {
         // If the challenge is newer than ones already found
-        if ((newc===null)||(newc.endsAt<c.endsAt)) {
+        if ((newc === null) || (newc.endsAt < c.endsAt)) {
           // Set the challenge variable to this challenge
           newc = c;
         }
@@ -161,11 +161,11 @@ export default Controller.extend({
       // Get the current user
       let cu = this.get('currentUser.user');
       if (cu) {
-        if (cu.currentDateInDateRange(newc.prepStartsAt,newc.endsAt)) {
-          if (newc.eventType==1) {
-            str = "<img src='/images/global/tent.svg' style='width: 36px; padding-bottom: 10px;' /> Welcome to Camp";
+        if (cu.currentDateInDateRange(newc.prepStartsAt, newc.endsAt)) {
+          if (newc.eventType == 1) {
+            str = "<img alt='drawing of a tent' src='/images/global/tent.svg' style='width: 36px; padding-bottom: 10px;' /> Welcome to Camp";
           } else {
-            str = "<img src='/images/global/helmet.svg' style='width: 36px; padding-bottom: 10px;' /> Welcome to " + newc.name;
+            str = "<img alt='graphic of a helmet' src='/images/global/helmet.svg' style='width: 36px; padding-bottom: 10px;' /> Welcome to " + newc.name;
           }
         }
       }
@@ -173,9 +173,9 @@ export default Controller.extend({
     // Return if they won or not
     return str;
   }),
-  
+
   // Returns true if user has won latest event, false if not
-  currentEventWon: computed('currentUser.user.projects.[]','primaryProject.currentProjectChallenge.count',function() {
+  currentEventWon: computed('currentUser.user.projects.[]', 'primaryProject.currentProjectChallenge.count', function () {
     // Set a variable to return
     let winner = false;
     // Set a local variable for the store
@@ -185,11 +185,11 @@ export default Controller.extend({
     // Set a local variable for the correct challenge
     let newc = null;
     // Loop through the challenges to find the latest event
-    cs.forEach(function(c) {
+    cs.forEach(function (c) {
       // If this is a November event
-      if (c.eventType==0) {
+      if (c.eventType == 0) {
         // If the challenge is newer than ones already found
-        if ((newc===null)||(newc.endsAt<c.endsAt)) {
+        if ((newc === null) || (newc.endsAt < c.endsAt)) {
           // Set the challenge variable to this challenge
           newc = c;
         }
@@ -204,17 +204,17 @@ export default Controller.extend({
         // Get all project_challenges
         let pcs = store.peekAll('project-challenge');
         // Loop through them
-        pcs.forEach(function(pc) {
+        pcs.forEach(function (pc) {
           // If this project challenge is for the latest event...
-          if (newc.id==pc.challenge_id) {
+          if (newc.id == pc.challenge_id) {
             // Find the associated project
-            let p = store.peekRecord('project',pc.project_id);
+            let p = store.peekRecord('project', pc.project_id);
             // If the project is found
             if (p) {
               // If the current user is the author
-              if (p.user_id==cuid) {
+              if (p.user_id == cuid) {
                 // if the goal has been met
-                if (pc.count>=pc.goal) {
+                if (pc.count >= pc.goal) {
                   // The user won
                   winner = true;
                 }
@@ -227,12 +227,12 @@ export default Controller.extend({
     // Return if they won or not
     return winner;
   }),
-  
-  
+
+
   queryParams: ['addProject'],
-  
+
   addProject: false,
-  
+
   actions: {
     afterProjectModalClose() {
       this.set('addProject', null);
