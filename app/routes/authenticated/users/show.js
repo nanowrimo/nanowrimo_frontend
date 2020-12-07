6060e68f9de910d42299cd9b7828de84f812c8ce
nanojezra <jezra@nanowrimo.org>
2020-12-07 14:04:12 -0800

    Removed 'projectSessions' as an include when requesting a user's profile

--- app/routes/authenticated/users/show.js
+++ app/routes/authenticated/users/show.js
@@ -3,7 +3,7 @@ import ScrollRoute from 'nanowrimo/routes/scroll-route'
 
 export default ScrollRoute.extend({
   model(params) {
-    return this.get('store').queryRecord('user', { name: params.slug, include: 'externalLinks,favoriteBooks,favoriteAuthors,projects,projectChallenges,projectSessions,user-badges' });
+    return this.get('store').queryRecord('user', { name: params.slug, include: 'externalLinks,favoriteBooks,favoriteAuthors,projects,projectChallenges,user-badges' });
   },
   setupController(controller, model) {
     this._super(...arguments);
