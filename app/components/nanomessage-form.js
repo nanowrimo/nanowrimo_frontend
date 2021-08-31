99128ff4d74c5b9c23302894a4a3568531a62588
nanodave <dave_beck@nanowrimo.org>
2021-08-31 12:59:38 -0700

    Re-aligned red dot on nav icon ref https://github.com/nanowrimo/Issues-and-Actions/issues/185

--- app/components/nanomessage-form.js
+++ app/components/nanomessage-form.js
@@ -13,17 +13,33 @@ export default Component.extend({
   adminIsChecked: null,
   emailIsChecked: null,
   isDisabled: false,
+  isSubmittedNoEmail: false,
   
+  init() {
+    this._super(...arguments);
+    let nm = this.get('store').createRecord('nanomessage');
+    let cu = this.get('currentUser.user');
+    nm.set('user',cu);
+    let g = this.get('group');
+    nm.set('group',g);
+    this.set('newNanomessage', nm);
+    // No email confirmation settings
+    this.set('cancelConfirmationYesText', 'Continue');
+    this.set('cancelConfirmationNoText', 'Cancel'); 
+    this.set('cancelConfirmationTitleText', 'Send without email?');
+    this.set('cancelConfirmationQuestion', `You didn't select the "Also send message by email" option. Would you like to continue and send this message without triggering an email send?`);
+  },
+
   _callAfterSubmit() {
     let callback = this.get('afterSubmit');
     if (callback) { callback(); }
   },
   
-  userIsAdmin: computed('currentUser.isLoaded}',function() {
+  userIsAdmin: computed('group','currentUser.isLoaded}',function() {
     let found = false;
+    let g = this.get('group');
     if (this.get('currentUser.isLoaded')) {
       let gus = this.get('store').peekAll('groupUser');
-      let g = this.get('group');
       let cu = this.get('currentUser.user');
       gus.forEach(function(gu) {
         if ((gu.group_id==g.id)&&(gu.user_id==cu.id)&&(gu.isAdmin)) {
@@ -38,35 +54,24 @@ export default Component.extend({
     let gt = this.get('group.groupType');
     return ((c!='nanomessages')&&(gt!='writing group'));
   }),
-  showForm: computed('context','group.groupType',function() {
+  
+  showForm: computed('context','group',function() {
     let c = this.get("context");
-    let t = this.get('group.groupType');
+    let t = this.get('group');
+    let gt = t.groupType;
     let showit = false;
     if (c!='nanomessages') {
       showit = true;
     } else {
-      if (t=='buddies') {
+      if (gt=='buddies') {
         showit = true;
       } else {
-        let uia = this.get('userIsAdmin');
-        if (uia) {
-          showit = true;
-        }
+        showit = this.get('userIsAdmin');
       }
     }
     return showit;
   }),
   
-  init() {
-    this._super(...arguments);
-    let nm = this.get('store').createRecord('nanomessage');
-    let cu = this.get('currentUser.user');
-    nm.set('user',cu);
-    let g = this.get('group');
-    nm.set('group',g);
-    this.set('newNanomessage', nm);
-  },
-
   iconClass: computed(function() {
     return "fa fa-paper-plane-o";
   }),
@@ -78,6 +83,21 @@ export default Component.extend({
     doEmailChecked(isChecked) {
       this.set('emailIsChecked',isChecked);
     },
+    
+    confirmCancel(){
+      //show the delete dialog
+      this.set('showConfirmCancel', true);
+    },
+    cancelConfirmationYes(){
+      this.doCancel();
+      //close the modal
+      this.set('showConfirmCancel', false);
+    },
+    cancelConfirmationNo(){
+      //close the modal
+      this.set('showConfirmCancel', false);
+    },
+    
     afterSubmit() {
       this.set('isDisabled',true);
       let i = document.querySelector('.medium-editor-element').innerHTML;
