24fd7b4ef16758d5132bad29ec50a7e6dfd4de56
nanojezra <jezra@nanowrimo.org>
2022-06-01 11:14:35 -0700

    Set api endpoint in config

--- config/environment.js
+++ config/environment.js
@@ -144,7 +144,7 @@ module.exports = function(environment) {
 
   //is this a staging target?
   if (process.env.TARGET=='staging' || environment==="dev-with-staging-api") {
-    ENV.APP.API_HOST = 'https://staging-vpc.api.nanowrimo.org';
+    ENV.APP.API_HOST = 'https://stagingvpc.api.nanowrimo.org';
     ENV.torii.providers['custom-google'].redirectUri = 'https://staging-vpc.nanowrimo.org/oauth2callback';
     ENV.forumsUrl = "https://staging.forums.nanowrimo.org";
   }
