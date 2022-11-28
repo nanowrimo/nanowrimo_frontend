89d344f6121f7f1ab88d0011b51024fa0fcab379
nanojezra <jezra@nanowrimo.org>
2022-11-28 10:36:41 -0800

    Modifed route to not check for a win
    the api checks for the win

--- app/routes/authenticated/nanowrimo-2022-winner.js
+++ app/routes/authenticated/nanowrimo-2022-winner.js
@@ -5,17 +5,14 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend({
   currentUser: service(),
+  session: service(),
   templateName: '404',
   model() {
-    let cu = this.get('currentUser.user');
-    let winner = cu.wonEventByName("NaNoWriMo 2022");
-    if (!winner) {
-      // return and the default 404 will be displayed
-      return;
-    }
-
-    let endpoint =  `${ENV.APP.API_HOST}/pages/nanowrimo-2022-winner?user_id=`+cu.get('id');
-    return fetch(endpoint).then((data)=>{
+    let { auth_token }  = this.get('session.data.authenticated');
+    let endpoint =  `${ENV.APP.API_HOST}/pages/nanowrimo-2022-winner`;
+    return fetch(endpoint,{
+      headers: { 'Content-Type': 'application/json', 'Authorization': auth_token}
+      }).then((data)=>{
       return data.json().then((json)=>{
         return json;
       });
