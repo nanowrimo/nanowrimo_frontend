3f0a3f6f560dbeadc8136a522a84f23816b4cf49
nanojezra <jezra@nanowrimo.org>
2023-02-21 14:55:45 -0800

    modified route to redirect after current user is loaded

--- app/routes/authenticated/groups.js
+++ app/routes/authenticated/groups.js
@@ -1,14 +1,18 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-
+import { observer } from '@ember/object';
 export default Route.extend({
   currentUser: service(),
+  
+  observeCurrentUser: observer('currentUser.user', function(){
+    let cu = this.get('currentUser.user');
+    if (cu) {
+      // redirect to the user's slug
+      this.transitionTo('authenticated.users.show.groups', cu.slug);
+    }
+  }),
+  
   model() {
-    let userSlug = this.get('currentUser.user.slug');
-    return userSlug;
-  },
-  afterModel(model) {
-    let us = model;
-    this.transitionTo('authenticated.users.show.groups', us);
+    let user = this.get('currentUser.user');
   }
 });
