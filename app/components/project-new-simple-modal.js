77d0814656e003200179e74ffe6cffb3c42bb041
nanodave <dave_beck@nanowrimo.org>
2023-03-27 14:30:03 -0700

    Removed Axios

--- app/components/project-new-simple-modal.js
+++ app/components/project-new-simple-modal.js
@@ -4,7 +4,6 @@ import { sort } from '@ember/object/computed';
 import { computed, observer }  from '@ember/object';
 import { inject as service } from '@ember/service';
 import moment from 'moment';
-import axios from 'axios';
 import ENV from 'nanowrimo/config/environment';
 
 export default Component.extend({
